67185845acbefa59c677283cf33dc7de0af30421
Mauricio Fernandez <mfernandezuk@hotmail.com>
2022-10-13 20:49:23 -0300

    fonts change

--- js/contact.js
+++ js/contact.js
@@ -1,29 +1,3 @@
-/* Close sidebar funcionality */
-let links = document.getElementsByClassName('link')
-for (const link of links)
-{
-    link.addEventListener('click', ()=>{
-
-        let check = document.getElementById('check');
-        check.checked = false;
-    });
-    
-}
-
-if(screen.width > 1240)
-{
-    let img = document.getElementById('foundress');
-    let imgUs = document.getElementById('usPhoto');
-    if(img)
-    {
-        img.setAttribute("src", "images/nancy-desktop2.png")
-    }
-    if(imgUs)
-    {
-        imgUs.setAttribute("src", "images/nancy-desktop-us2.png")
-    }
-}
-
 // emailjs
 const notification = (text) =>
 {
@@ -60,5 +34,4 @@ emailjs.sendForm(serviceID, templateID, this)
     notification('¡Ups, error al enviar!');
     console.log(JSON.stringify(err));
     });
-
 });
